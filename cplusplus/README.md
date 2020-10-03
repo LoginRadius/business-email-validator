1cf64e127ad770b748ffedca834576401387dda3
William Zhang <WilliamHYZhang@gmail.com>
2020-10-02 22:25:27 -0400

    feat: implement cpp wrapper

---
+++ cplusplus/README.md
@@ -0,0 +1,9 @@
+## Business Email Validator
+
+### About
+This is the library wrapper for C++.
+
+### Usage
+
+### Credit
+Implemented by [William Zhang](https://github.com/WilliamHYZhang), October 2020.
---
+++ cplusplus/wrapper.hpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+#include "json.hpp"
+using json = nlohmann::json;
+
+#include <fstream>
+#include <sstream>
+
+
+
+std::vector<std::string> split (const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss (s);
+    std::string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+    return result;
+}
+
+namespace wrapper {
+  bool is_free(std::string email){
+    std::ifstream fin("free_email_service.json");
+    json domains = json::parse(fin);
+
+    std::vector<std::string> v = split(email, '@');
+    if(v.size() != 2) return false;
+    return domains.find(v[1]) != domains.end();
+  }
+}
+

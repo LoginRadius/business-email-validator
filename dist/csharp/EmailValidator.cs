b1bea207a3450776cc8cf1ad2ec851020042844c
hemant404 <56862469+hemant404@users.noreply.github.com>
2020-10-05 14:10:39 +0530

    Added error into the CheckFreeEmailDomain method.

--- dist/csharp/EmailValidator.cs
+++ dist/csharp/EmailValidator.cs
@@ -8,6 +8,9 @@ namespace BusinessEmailValidator
 {
     public class EmailValidator
     {
+        /// <summary>
+        /// Load list of free email domains from the json file
+        /// </summary>
         public static Dictionary<string, bool> LoadJson()
         {
             Dictionary<string, bool> domainListItems = null;
@@ -19,20 +22,34 @@ namespace BusinessEmailValidator
             return domainListItems;
         }
 
-        public static bool CheckFreeEmailDomain(string emailId)
+        /// <summary>
+        /// Check for the free email domain from the emailId
+        /// </summary>
+        /// <param name="emailId">EmailId of the user</param>
+        /// <param name="error">Error from the method</param>
+        
+        public static bool CheckFreeEmailDomain(string emailId,out string error)
         {
+            error = "Not a free email domain";
             try
             {
+                //Check for a valid email address
                 var emailDomain = new MailAddress(emailId).Host;
+                //Load FreeEmailDomain list
                 var domainList = LoadJson();
                 foreach (var domain in domainList)
                 {
+                    //Check for free email domain
                     if (emailDomain == domain.Key)
+                    {
+                        error = "Free email domain";
                         return true;
+                    }
                 }
             }
             catch (FormatException)
             {
+                error = "Not a valid email address";
                 return false;
             }
             return false;

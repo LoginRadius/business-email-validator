95a2bca7e1e922b2b4824deec015af0f6c03aa1d
hemant404 <56862469+hemant404@users.noreply.github.com>
2020-10-05 13:51:36 +0530

    Added chsrp file to validate email domain.

---
+++ dist/csharp/EmailValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using Newtonsoft.Json;
+
+namespace BusinessEmailValidator
+{
+    public class EmailValidator
+    {
+        public static Dictionary<string, bool> LoadJson()
+        {
+            Dictionary<string, bool> domainListItems = null;
+            using (StreamReader r = new StreamReader(@"src\freeEmailService.json"))
+            {
+                string json = r.ReadToEnd();
+                domainListItems = JsonConvert.DeserializeObject<Dictionary<string,bool>>(json);
+            }
+            return domainListItems;
+        }
+
+        public static bool CheckFreeEmailDomain(string emailId)
+        {
+            try
+            {
+                var emailDomain = new MailAddress(emailId).Host;
+                var domainList = LoadJson();
+                foreach (var domain in domainList)
+                {
+                    if (emailDomain == domain.Key)
+                        return true;
+                }
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+}

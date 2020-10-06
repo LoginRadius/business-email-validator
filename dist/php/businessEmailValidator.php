870675820c4e10ba375118b8ee1d4db988414421
Hajro <hajroahmetasevic92@gmail.com>
2020-10-06 10:37:56 +0200

    Code ident

--- dist/php/businessEmailValidator.php
+++ dist/php/businessEmailValidator.php
@@ -2,28 +2,29 @@
 
 class BusinessEmailValidator
 {
-	/**
-	* Used to check if provided email address is in list of blocked domains
-	* @access public
-	* @param string $email
-	* The email address we want to check
-	* @example 'john@doe.com'
-	* @return bool
-	*/
-	public function isBlocked($email = '') {
-		try {
+    /**
+     * Used to check if provided email address is in list of blocked domains
+     * @access public
+     * @param string $email
+     * The email address we want to check
+     * @return bool
+     * @example 'john@doe.com'
+     */
+    public function isBlocked($email = '')
+    {
+        try {
 
-		    // Domains list path
+            // Domains list path
             $path = 'src/freeEmaislService.json';
 
             // Check if file exist
-            if(file_exists($path) === FALSE) throw new Exception("error: File '{$path}' not found!");
+            if (file_exists($path) === FALSE) throw new Exception("error: File '{$path}' not found!");
 
             // Check if provided email is valid
-            if(filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) throw new Exception("error: Email address '{$email}' is not valid!");
+            if (filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) throw new Exception("error: Email address '{$email}' is not valid!");
 
             // Convert json string to array
-            $list_blocked = (array) json_decode(file_get_contents($path), true);
+            $list_blocked = (array)json_decode(file_get_contents($path), true);
 
             // Get domain from email address
             $email_split = explode('@', $email);
@@ -31,7 +32,7 @@ class BusinessEmailValidator
 
             // Loop list to search for an email inside
             foreach ($list_blocked as $domain => $blocked) {
-                if($email_domain === $domain) {
+                if ($email_domain === $domain) {
                     // email is blocked
                     return true;
                 }
@@ -39,9 +40,9 @@ class BusinessEmailValidator
             // Email's domain is not blocked
             return false;
 
-        }catch (Exception $e){
+        } catch (Exception $e) {
             return false;
         }
 
-	}
+    }
 }
